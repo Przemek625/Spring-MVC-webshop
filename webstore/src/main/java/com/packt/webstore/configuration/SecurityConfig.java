f55acdc61cee3edd3c04925564da35f0be561e0a
Przemek <przemek625@gmail.com>
2016-07-27 01:15:55 +0200

    Working on Security funcionality. PART1

---
+++ webstore/src/main/java/com/packt/webstore/configuration/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.packt.webstore.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import javax.sql.DataSource;
+/**
+ * Created by Przemek on 2016-07-26.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().
+                antMatchers("/customers/add").authenticated();
+    }
+}
---
+++ webstore/src/main/java/com/packt/webstore/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.packt.webstore.controller;
+
+/**
+ * Created by Przemek on 2016-07-25.
+ */
+
+public class LoginController {
+}
