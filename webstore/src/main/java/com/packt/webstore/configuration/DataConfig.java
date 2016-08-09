6b64347656c650952d659cbcc95526c097c102d7
Przemek <przemek625@gmail.com>
2016-08-09 17:27:07 +0200

    I moved the dataSource properties to jdbc.properties file

--- webstore/src/main/java/com/packt/webstore/configuration/DataConfig.java
+++ webstore/src/main/java/com/packt/webstore/configuration/DataConfig.java
@@ -1,23 +1,44 @@
 package com.packt.webstore.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 /**
  * Created by Przemek on 2016-07-27.
  */
 @Configuration
+@PropertySource(value = { "classpath:jdbc.properties" })
 public class DataConfig {
 
+
+    @Value("${jdbc.className}")
+    private String className;
+
+    @Value("${jdbc.url}")
+    private String url;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean
     public DriverManagerDataSource dataSource(){
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/sys");
-        driverManagerDataSource.setUsername("root");
-        driverManagerDataSource.setPassword("root");
+        driverManagerDataSource.setDriverClassName(className);
+        driverManagerDataSource.setUrl(url);
+        driverManagerDataSource.setUsername(username);
+        driverManagerDataSource.setPassword(password);
         return driverManagerDataSource;
     }
 
