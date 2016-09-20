7fa13a2fff10b0e9db71abae06ea4a239129fec8
Przemek <przemek625@gmail.com>
2016-09-20 22:27:06 +0200

    Caching#3

--- webstore/src/main/java/com/packt/webstore/domain/repository/CustomerRepository.java
+++ webstore/src/main/java/com/packt/webstore/domain/repository/CustomerRepository.java
@@ -1,12 +1,15 @@
 package com.packt.webstore.domain.repository;
 
 import com.packt.webstore.domain.Customer;
+import org.springframework.cache.annotation.Cacheable;
+
 import java.util.List;
 /**
  * Created by Przemek on 2016-07-19.
  */
 public interface CustomerRepository {
-
+    
+    @Cacheable("customersCache")
     List<Customer> getAllCustomers();
 
     void deleteCustomer(int id );
