34a2e3315128eda82ee7e52eea11ff292b9533bc
Przemek <przemek625@gmail.com>
2016-09-25 15:28:28 +0200

    Caching#4

--- webstore/src/main/java/com/packt/webstore/domain/repository/CustomerRepository.java
+++ webstore/src/main/java/com/packt/webstore/domain/repository/CustomerRepository.java
@@ -1,6 +1,7 @@
 package com.packt.webstore.domain.repository;
 
 import com.packt.webstore.domain.Customer;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
@@ -8,14 +9,17 @@ import java.util.List;
  * Created by Przemek on 2016-07-19.
  */
 public interface CustomerRepository {
-    
-    @Cacheable("customersCache")
+
+    @Cacheable(cacheNames = "customersCache")
     List<Customer> getAllCustomers();
 
+    @CacheEvict(cacheNames = "customersCache",allEntries = true)
     void deleteCustomer(int id );
 
+    @CacheEvict(cacheNames = "customersCache",allEntries = true)
     int addCustomer(Customer customer);
 
+    @CacheEvict(cacheNames = "customersCache",allEntries = true)
     void updateCustomer(Customer customer, int id);
 
     boolean customerExists(int id);
